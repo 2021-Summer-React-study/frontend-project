fe6d60eedf76b1660229ddab9e88da02e7320a72
Jsgithubchannel <jsschannel99@gmail.com>
2021-09-29 00:29:39 +0900

    Feat: add Redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./modules/index";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer, composeWithDevTools());
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
